29f8b476a0cfe36ab904b13de93d4e6365e27c7b
blake morjal <bmorjal@students.mchenry.edu>
2022-05-23 19:39:06 -0500

    Assessment01

---
+++ assessment-01/script/script.js
@@ -0,0 +1,16 @@
+function myFunction2()
+    {document.getElementById("sample").innerHTML = "The nature is different colors";
+}
+
+function resizeImage(){
+    var image = document.getElementById('one');
+    image.style.width="300px";
+    image.style.height="300px";
+}
+
+function resetImage(){
+    var image = document.getElementById('one');
+    image.style.width="200px";
+    image.style.height="200px";
+}
+
